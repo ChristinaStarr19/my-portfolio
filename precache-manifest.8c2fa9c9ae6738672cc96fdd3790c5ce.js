64e7f455b751260538fa244a8146d195c14839f1
Christina Starr <christinastarr19@gmail.com>
2020-08-06 09:34:19 -0400

    Updates

---
+++ precache-manifest.8c2fa9c9ae6738672cc96fdd3790c5ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08fad2cd9da92a8d236d175b0632ccf",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "98357ac6f8d254a2f7dc",
+    "url": "/my-portfolio/static/css/main.f75ef9c5.chunk.css"
+  },
+  {
+    "revision": "76f118612a537a5f2f19",
+    "url": "/my-portfolio/static/js/2.f9b3c463.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.f9b3c463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98357ac6f8d254a2f7dc",
+    "url": "/my-portfolio/static/js/main.5a4de749.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
