ce3eb5f67f3d6159bd9bd3249637ec69896d5257
Christina Starr <christinastarr19@gmail.com>
2020-08-05 22:32:07 -0400

    changed class to classname, a to link and href to to.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Aboutme from "./components/Aboutme";
+
 
 function App() {
   return (
-    <div className="App">
-      
-      {/* <!-- Navigation Bar --> */}
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Aboutme} />
+        </Switch>
 
-{/* <!-- Changed nav bar color in css using class="bg-light" --> */}
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-
-    {/* <!-- Clicking name will take user to About Me page --> */}
-    <a class="navbar-brand heading" href="index.html">Christina C. Starr</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ml-auto">
-
-  {/* <!-- Named and linked About Me Page --> */}
-        <li class="nav-item active heading">
-          <a class="nav-link" href="index.html">About Me<span class="sr-only">(current)</span></a>
-        </li>
-        
-  {/* <!-- Named and linked Portfolio Page --> */}
-        <li class="nav-item active heading">
-        <a class="nav-link" href="portfolio.html">Portfolio<span class="sr-only">(current)</span></a>
-        </li>
-
-  {/* <!-- Named and linked Contact Page --> */}
-       <li class="nav-item active heading">
-       <a class="nav-link" href="contact.html">Contact<span class="sr-only">(current)</span></a>
-      </li>
-      
-         {/* <!-- Named and linked Resume Page --> */}
-         <li class="nav-item active heading">
-          <a class="nav-link" href="resume.html">Resume<span class="sr-only">(current)</span></a>
-         </li>
-     </ul>
-    </div>
-  </nav>
-     <p>Hello World</p>
-    </div>
+        <p>Hello World</p>
+      </div>
+    </Router>
   );
 }
 
